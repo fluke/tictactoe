29e97f4f05e8d25ebdb189c9a6697005d2d18716
Kartik Luke Singh <kartikluke@gmail.com>
2021-05-01 17:03:25 +0530

    Fix grid orientation

--- services/print_grid_state.rb
+++ services/print_grid_state.rb
@@ -5,8 +5,8 @@ module TicTacToe
     end
 
     def call
-      (0..2).each do |x|
-        (0..2).each do |y|
+      (2).downto(0).each do |y|
+        (0..2).each do |x|
           print @state[x][y]
         end
         puts
