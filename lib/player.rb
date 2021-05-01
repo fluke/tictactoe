2be0a41779c9c644fbc3d46bf9a859cccbbbdd3a
Kartik Luke Singh <kartikluke@gmail.com>
2021-05-01 15:25:47 +0530

    Add Player class

---
+++ lib/player.rb
@@ -0,0 +1,3 @@
+module TicTacToe
+  Player = Struct.new(:name, :symbol)
+end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,5 +1,20 @@
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/services/*.rb'].each { |file| require file }
 
-grid = TicTacToe::Grid.new
-TicTacToe::PrintGridState.new(grid.state).call
+module TicTacToe
+  class Game
+    def self.start
+      # Create new grid
+      grid = Grid.new
+      PrintGridState.new(grid.state).call
+
+      puts "Name for Player 1"
+      player1 = Player.new(name: gets.chomp, symbol: Grid::CROSS)
+
+      puts "Name for Player 2"
+      player2 = Player.new(name: gets.chomp, symbol: Grid::CROSS)
+    end
+  end
+end
+
+TicTacToe::Game.start
